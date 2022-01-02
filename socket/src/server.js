ed82f423ed16bd692fcc235be3c1bc8a4dfdb585
BeADia <koc0819@gmail.com>
2022-01-02 16:11:01 +0900

    change socket port to 4000

--- socket/src/server.js
+++ socket/src/server.js
@@ -4,17 +4,17 @@ const cors = require("cors");
 const axios = require("axios");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
 const http = require("http");
-const port = process.env.PORT || 5000;
+const port = 4000;
 const Index = "/index.html";
 app.set("port", port);
 
 //https://jinhyukoo.github.io/js/2020/12/13/peerJS%EC%82%AC%EC%9A%A9%ED%95%B4%EB%B3%B4%EA%B8%B0.html
 app.get("/", (req, res) => {
-  res.sendFile(Index, { root: __dirname });
+    res.sendFile(Index, {root: __dirname});
 });
 
 //경원
@@ -33,48 +33,48 @@ app.get("/", (req, res) => {
 // ID : Mo_d0dmRjBOaHTb6qMqc
 // secret : xsRNSeVZUN
 app.post("/stt", async (req, res) => {
-  try {
-    const result = await axios({
-      method: "POST",
-      url: "https://openapi.naver.com/v1/papago/n2mt",
-      data: {
-        source: req.body.source,
-        target: req.body.target,
-        text: req.body.text,
-      },
-      headers: {
-        "X-Naver-Client-Id": "Mo_d0dmRjBOaHTb6qMqc",
-        "X-Naver-Client-Secret": "xsRNSeVZUN",
-        Accept: "application/json",
-      },
-    });
+    try {
+        const result = await axios({
+            method: "POST",
+            url: "https://openapi.naver.com/v1/papago/n2mt",
+            data: {
+                source: req.body.source,
+                target: req.body.target,
+                text: req.body.text,
+            },
+            headers: {
+                "X-Naver-Client-Id": "Mo_d0dmRjBOaHTb6qMqc",
+                "X-Naver-Client-Secret": "xsRNSeVZUN",
+                Accept: "application/json",
+            },
+        });
 
-    const resultText = result.data.message.result.translatedText;
-    res.send(resultText);
-  } catch (err) {
-    res.send(err);
-  }
+        const resultText = result.data.message.result.translatedText;
+        res.send(resultText);
+    } catch (err) {
+        res.send(err);
+    }
 
-  res.status(200);
+    res.status(200);
 });
 //끝
 const server = http.createServer(app);
 server.listen(port, () => console.log(`listen in ${port} port`));
 const io = require("socket.io")(server, {
-  cors: {
-    origin: ["http://localhost:3000", "https://capstone-925e4.web.app"],
-    methods: ["GET", "POST"],
-  },
+    cors: {
+        origin: ["http://localhost:3000", "https://capstone-925e4.web.app"],
+        methods: ["GET", "POST"],
+    },
 });
 io.on("connection", socket => {
-  console.log("[connected from]", socket.id, new Date());
-  socket.on("join-room", (roomId, userId) => {
-    console.log(roomId, userId);
-    socket.join(roomId);
-    socket.to(roomId).emit("user-connected", userId);
+    console.log("[connected from]", socket.id, new Date());
+    socket.on("join-room", (roomId, userId) => {
+        console.log(roomId, userId);
+        socket.join(roomId);
+        socket.to(roomId).emit("user-connected", userId);
 
-    socket.on("disconnect", () => {
-      socket.to(roomId).emit("user-disconnected", userId);
+        socket.on("disconnect", () => {
+            socket.to(roomId).emit("user-disconnected", userId);
+        });
     });
-  });
 });
--- web/src/components/views/LiveLecture/Video/connect.ts
+++ web/src/components/views/LiveLecture/Video/connect.ts
@@ -1,110 +1,114 @@
-import { makeAlarm } from "./../../../utils/Alarm";
+import {makeAlarm} from "./../../../utils/Alarm";
 import io from "socket.io-client";
 import Peer from "peerjs";
+
 type PeerList = {
-  [userId: string]: Peer.MediaConnection;
+    [userId: string]: Peer.MediaConnection;
 };
-const serverURL = "https://capstone-ontact.herokuapp.com";
+const serverURL = "13.209.243.231:4000";
 let peerList: PeerList = {};
 let peerIds: string[] = [];
 let socket;
 export const getSocket = () => {
-  if (socket === undefined) {
-    socket = io(serverURL, {
-      transports: ["polling"],
-    });
-    console.log("get socket", socket);
-  }
-  return socket;
+    if (socket === undefined) {
+        socket = io(serverURL, {
+            transports: ["polling"],
+        });
+        console.log("get socket", socket);
+    }
+    return socket;
 };
 export const educateeConnect = (
-  localId: string,
-  stream: MediaStream,
-  remoteVideoRef: React.RefObject<HTMLVideoElement>,
-  lectureName: string,
-  profId: string
+    localId: string,
+    stream: MediaStream,
+    remoteVideoRef: React.RefObject<HTMLVideoElement>,
+    lectureName: string,
+    profId: string
 ) => {
-  getSocket();
-  let peer;
-  try {
-    peer = new Peer(localId);
-  } catch (e) {
-    console.log(e);
-  }
-  peer.on("open", id => {
-    // id : localid
-    console.log("[open]", id);
-    socket.emit("join-room", lectureName, id);
-  });
-  peer.on("call", call => {
-    call.answer(stream);
-    call.on("stream", remoteStream => {
-      console.log("get stream !!!");
-      if (remoteVideoRef.current) {
-        remoteVideoRef.current.srcObject = remoteStream;
-      }
+    getSocket();
+    let peer;
+    try {
+        peer = new Peer(localId);
+    } catch (e) {
+        console.log(e);
+    }
+    peer.on("open", id => {
+        // id : localid
+        console.log("[open]", id);
+        socket.emit("join-room", lectureName, id);
     });
-  });
-  socket.on("user-connected", (userId: string) => {
-    //steam 수신 peer 연결
-    // userid : 상대방 아이디
-    console.log("connected with :", userId);
-    const call = peer.call(profId, stream);
-    // const call = peer.call(profId, stream);
-    call.on("stream", remoteStream => {
-      if (remoteVideoRef.current) {
-        remoteVideoRef.current.srcObject = remoteStream;
-      }
+    peer.on("call", call => {
+        call.answer(stream);
+        call.on("stream", remoteStream => {
+            console.log("get stream !!!");
+            if (remoteVideoRef.current) {
+                remoteVideoRef.current.srcObject = remoteStream;
+            }
+        });
     });
-    call.on("close", () => {
-      if (remoteVideoRef.current) {
-        remoteVideoRef.current.remove();
-      }
+    socket.on("user-connected", (userId: string) => {
+        //steam 수신 peer 연결
+        // userid : 상대방 아이디
+        console.log("connected with :", userId);
+        const call = peer.call(profId, stream);
+        // const call = peer.call(profId, stream);
+        call.on("stream", remoteStream => {
+            if (remoteVideoRef.current) {
+                remoteVideoRef.current.srcObject = remoteStream;
+            }
+        });
+        call.on("close", () => {
+            if (remoteVideoRef.current) {
+                remoteVideoRef.current.remove();
+            }
+        });
+        peerList[userId] = call;
+        peerIds.push(userId);
+        console.log(`total connected peer = ${peerIds}`);
     });
-    peerList[userId] = call;
-    peerIds.push(userId);
-    console.log(`total connected peer = ${peerIds}`);
-  });
 };
 export const educatorConnect = (
-  localId: string,
-  localStream: MediaStream,
-  localVideoRef: React.RefObject<HTMLVideoElement>,
-  lectureName: string,
-  addPeers: (val: string) => void
+    localId: string,
+    localStream: MediaStream,
+    localVideoRef: React.RefObject<HTMLVideoElement>,
+    lectureName: string,
+    addPeers: (val: string) => void
 ) => {
-  getSocket();
-  let peer;
-  try {
-    peer = new Peer(localId);
-  } catch (e) {
-    console.log(e);
-  }
-  if (localVideoRef.current) {
-    localVideoRef.current.srcObject = localStream;
-  }
-  peer.on("open", id => {
-    // id : localid
-    console.log("[open]", id);
-    socket.emit("join-room", lectureName, id);
-  });
-  peer.on("call", call => {
-    call.answer(localStream);
-    call.on("stream", remoteStream => {});
-  });
+    getSocket();
+    let peer;
+    try {
+        peer = new Peer(localId);
+    } catch (e) {
+        console.log(e);
+    }
+    if (localVideoRef.current) {
+        localVideoRef.current.srcObject = localStream;
+    }
+    peer.on("open", id => {
+        // id : localid
+        console.log("[open]", id);
+        socket.emit("join-room", lectureName, id);
+    });
+    peer.on("call", call => {
+        call.answer(localStream);
+        call.on("stream", remoteStream => {
+        });
+    });
 
-  socket.on("user-connected", userId => {
-    makeAlarm(`${userId}님이 입장했습니다.`, lectureName);
+    socket.on("user-connected", userId => {
+        makeAlarm(`${userId}님이 입장했습니다.`, lectureName);
 
-    //steam 수신 peer 연결
-    // userid : 상대방 아이디
-    console.log("connected with :", userId);
-    const call = peer.call(userId, localStream);
-    call.on("stream", remoteStream => {});
-    call.on("close", () => {});
-    peerList[userId] = call;
-    peerIds.push(userId);
-    addPeers(userId);
-    console.log(`total connected peer = ${peerIds}`);
-  });
+        //steam 수신 peer 연결
+        // userid : 상대방 아이디
+        console.log("connected with :", userId);
+        const call = peer.call(userId, localStream);
+        call.on("stream", remoteStream => {
+        });
+        call.on("close", () => {
+        });
+        peerList[userId] = call;
+        peerIds.push(userId);
+        addPeers(userId);
+        console.log(`total connected peer = ${peerIds}`);
+    });
 };
